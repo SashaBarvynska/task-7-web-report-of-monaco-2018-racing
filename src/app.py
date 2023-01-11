32ace080d32fb0c52399b2b2bfe0a8bc06c6a9bd
Sasha Barvynska <alexbarvynska@gmail.com>
2023-01-11 12:59:32 +0200

    changed path to folder's files

--- src/app.py
+++ src/app.py
@@ -23,7 +23,7 @@ def show_drivers():
 
 
 def get_drivers():
-    file_start, file_end, abbreviations_file = Files.find_files('D:\\Sasha\\Projects\\task_7\\data_files')
+    file_start, file_end, abbreviations_file = Files.find_files('D:\\Sasha\\Projects\\task-7-web-report-of-monaco-2018-racing\\data_files')
     list_drivers = Drivers.build_report(
         FormatFile.format_file_abbreviation_data(Files.open_files(abbreviations_file)),
         FormatFile.format_file_time(Files.open_files(file_start)),
