97549f9f3359909047614a7effb2e5c99c79d143
Sasha Barvynska <alexbarvynska@gmail.com>
2023-02-02 04:07:50 +0200

    mocked tests

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -2,12 +2,13 @@ from unittest.mock import patch
 
 import pytest
 
-from src.controller import DriverAdaptor
+from src.controller import DriverAdaptor, get_drivers
 from tests.constants import (DICT_ABB, DICT_TIME, DRIVER_DRR, DRIVER_SVF,
                              LIST_DRIVERS, lIST_ALL_DRIVERS_ASC,
                              lIST_ALL_DRIVERS_DESC)
 
 
+@patch("src.controller.get_drivers", return_value=lIST_ALL_DRIVERS_ASC)
 @pytest.mark.parametrize("key,value,expected", [
     (
         "abbreviation",
@@ -20,38 +21,39 @@ from tests.constants import (DICT_ABB, DICT_TIME, DRIVER_DRR, DRIVER_SVF,
         DRIVER_SVF
     ),
 ])
-def test_get_driver(key, value, expected):
+def test_get_driver(mock_get_drivers, key, value, expected):
     instance = DriverAdaptor()
+    mock_get_drivers.assert_called_once()
     assert instance.get_driver(key, value) == expected
 
 
 @patch(
-    "task_Barvynska.files.Files.find_files",
+    "src.controller.Files.find_files",
     return_value=["path_1", "path_2", "path_3"],
 )
 @patch(
-    "task_Barvynska.files.Files.open_files",
+    "src.controller.Files.open_files",
     return_value="file_content",
 )
 @patch(
-    "task_Barvynska.format_file.FormatFile.format_file_abbreviation_data",
+    "src.controller.FormatFile.format_file_abbreviation_data",
     return_value=DICT_ABB,
 )
 @patch(
-    "task_Barvynska.format_file.FormatFile.format_file_time",
+    "src.controller.FormatFile.format_file_time",
     return_value=DICT_TIME,
 )
-@patch("task_Barvynska.drivers.Drivers.build_report", return_value=LIST_DRIVERS)
+@patch("src.controller.Drivers.build_report", return_value=LIST_DRIVERS)
 def test_get_drivers(mock_build_report, mock_format_file_time, mock_format_file_abbreviation_data, mock_open_files, mock_find_files):
-    instance = DriverAdaptor()
-    assert instance.list_drivers == LIST_DRIVERS
-    mock_build_report.assert_called_with(DICT_ABB, DICT_TIME, DICT_TIME)
-    mock_format_file_time.assert_called_with("file_content")
-    mock_format_file_abbreviation_data.assert_called_with("file_content")
-    mock_open_files.assert_called_with("path_2")
+    assert get_drivers() == LIST_DRIVERS
     mock_find_files.assert_called_with("data_files")
+    mock_open_files.assert_called_with("path_2")
+    mock_format_file_abbreviation_data.assert_called_with("file_content")
+    mock_format_file_time.assert_called_with("file_content")
+    mock_build_report.assert_called_with(DICT_ABB, DICT_TIME, DICT_TIME)
 
 
+@patch("src.controller.get_drivers", return_value=lIST_ALL_DRIVERS_ASC)
 @pytest.mark.parametrize("test_input,expected", [
     (
         True,
@@ -62,6 +64,7 @@ def test_get_drivers(mock_build_report, mock_format_file_time, mock_format_file_
         lIST_ALL_DRIVERS_DESC
     ),
 ])
-def test_sort_data(test_input, expected):
+def test_sort_data(mock_get_drivers, test_input, expected):
     instance = DriverAdaptor()
     assert instance.sort_data(test_input) == expected
+    mock_get_drivers.assert_called_once()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,9 +3,11 @@ from unittest.mock import patch
 import pytest
 from flask import Response
 
-from tests.constants import ASC_SVF, DESC_SVF, DRIVER_DRR, DRR
+from tests.constants import (ASC_SVF, DESC_SVF, DRIVER_DRR, DRR,
+                             lIST_ALL_DRIVERS_ASC)
 
 
+@patch("src.controller.get_drivers", return_value=lIST_ALL_DRIVERS_ASC)
 @pytest.mark.parametrize("order, sort", [
     (
         "desc", DESC_SVF
@@ -14,20 +16,22 @@ from tests.constants import ASC_SVF, DESC_SVF, DRIVER_DRR, DRR
         "asc", ASC_SVF
     ),
     ])
-def test_show_report(order, sort, client):
+def test_show_report(mock_get_drivers, order, sort, client):
     response: Response = client.get(f"/report?order={order}")
     assert sort in response.data.decode()
     assert response.status_code == 200
 
 
-def test_show_drivers(client):
+@patch("src.controller.get_drivers", return_value=lIST_ALL_DRIVERS_ASC)
+def test_show_drivers(mock_get_drivers, client):
     response: Response = client.get("/report/drivers")
     assert DRR in response.data.decode()
     assert response.status_code == 200
 
 
+@patch("src.controller.get_drivers", return_value=lIST_ALL_DRIVERS_ASC)
 @patch("src.routes.DriverAdaptor.get_driver", return_value=DRIVER_DRR)
-def test_get_info(mock_get_driver, client):
+def test_get_info(mock_get_driver, mock_get_drivers, client):
     response: Response = client.get("/report/drivers/DRR")
     assert "Daniel Ricciardo" in response.data.decode()
     assert response.status_code == 200
